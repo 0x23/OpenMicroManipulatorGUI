e90d96a202a54dda3615921c349d07f6206e2e1d
0x23 <no@email.com>
2026-09-18 15:11:41 +0200

    added missing file

---
+++ source/version.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------------------------------------
+# Project: OpenMicroManipulator
+# License: MIT (see LICENSE file for full description)
+#          All text in here must be included in any redistribution.
+# Author:  M. S. (diffraction limited)
+# --------------------------------------------------------------------------------------
+
+__version__ = "0.1.3"
